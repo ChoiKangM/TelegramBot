974d7131d15696d91e4f6399b8f5e266ed7e819a
kei01138 <choikm3847@gmail.com>
2019-05-30 16:27:02 +0900

    7th hour

--- README.md
+++ README.md
@@ -33,7 +33,36 @@ win 10에서 ngrok이 잘 안되는 경우
 
 ## 5교시
 `메아리치는 봇`
-`request.args` # GET
-`request.forms` # POST
-`request.get_json()` 
+  - `request.args` # GET
+  - `request.forms` # POST
+  - `request.get_json()` 
+
+## 6교시
+`로또 외치면 로또 번호`
+
+`is` vs `==`
+```python
+1000 is 10**3 # FALSE
+
+# 띄어쓰기 없는 짧은 문자의 경우는 다를 수 있다
+a = 'lotto lotto'
+b = 'lotto lotto'
+id(a) # 4437766384
+id(b) # 4437766448
+
+a is b # FALSE
+
+-5 ~ 255 # 자주 쓰는 숫자는 메모리에 이미 넣어둠
+```
+
+[네이버 개발자센터`Papago`](https://developers.naver.com/products/translator/)
+
+## 7교시
+`JSON`에서 번역할 문자열 찾기
+  - `get('message').get('result').get('translatedText')`
+`명령어 추가`
+  - `text[0:4] == '/번역 '`
+
+[`pythonanywhere`](https://www.pythonanywhere.com/)
+
 
--- bot/app.py
+++ bot/app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request
-import requests
+import requests, random
 app = Flask(__name__)
 
+# Telegram
 token = '815705291:AAE8jYAR3hS2A8V6s2XZgbDfOhbru7VwAnA'
 api_url = f'https://api.telegram.org/bot{token}'
+
+# Naver
+NAVER_CLIENT_ID = 'O75ucAQW4vx4GrJTH_R7'
+NAVER_CLIENT_SECRET = 'ijSXmbKN66'
+papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+
 
 @app.route(f'/{token}', methods=["POST"])
 def telegram():
@@ -20,12 +27,32 @@ def telegram():
   if message is not None:
     chat_id = message.get('from').get('id')
     text = message.get('text')
+    # 1. 로또
+    if text == "로또":
+      text = random.sample(range(1,46),6)
+    # 2. 네이버 번역
+    # /번역 안녕하세요!
+    if text[0:4] == '/번역 ':   
+      headers = {
+        'X-Naver-Client-id': NAVER_CLIENT_ID,
+        'X-Naver-Client-Secret': NAVER_CLIENT_SECRET
+      }
+      data = {
+        'source': 'ko',
+        'target': 'en',
+        'text': text[4:]
+      }
+      papago_res = requests.post(papago_url, headers=headers, data=data)
+      text = papago_res.json().get('message').get('result').get('translatedText')
+      
+      
+      
+      
     send_url = f'{api_url}/sendMessage?chat_id={chat_id}&text={text}'
-    
+      
     # sendMessage 요청 보내기
     response = requests.get(send_url)
   
-
   return '', 200 # body, status_code
 
 if __name__ == '__main__':
